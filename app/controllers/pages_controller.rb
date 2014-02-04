9afa3bb6ebda00286bf228f42564be3294aeb44d
= <k1016113@kingston.ac.uk>
2014-02-04 19:22:38 +0000

    added basic menu structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def home
+  @title = "The University Resource Centre For The Blind"
   end
+  
 
   def about
+  @title = "About Us"
   end
 
   def contact
+  @title = "Contact Us"
   end
 end
