1b81a0e00f0521a8397a64974bbe7c19cf8cfd27
= <k1016113@kingston.ac.uk>
2014-02-06 15:59:16 +0000

    fixed application layout file and added titles to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,17 @@
 class UserController < ApplicationController
   def home
+  @title = "Your Home Page"
   end
 
   def register
+  @title = "Register a New Account"
   end
 
   def login
+  @title = "Login"
   end
 
   def logout
+  @title = "You have been logged out"
   end
 end
